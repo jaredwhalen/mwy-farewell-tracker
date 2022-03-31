431d069a87d053d798b20cb12e35b61994958cfc
Jared Whalen <jared.m.whalen@gmail.com>
2022-03-31 11:19:16 -0400

    Update!

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,6 @@ message="${1:-Update!}"
 
 git pull
 
-cat concerts.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > src/data/concerts.json
 npm run build
 
 git add .
