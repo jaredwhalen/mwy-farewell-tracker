144adc0f7f6a1494798fd247c3d85094ac0358b8
Jared Whalen <jared.m.whalen@gmail.com>
2022-05-14 09:32:36 -0400

    Update!

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -12,7 +12,7 @@
   <p>To visualize this assortment (and just as an opportunity to geek out about an amazing band), I am using <a target="_blank" src="https://www.setlist.fm/setlists/mewithoutyou-43d60b1b.html">setlist.fm</a> data to visualize what songs the band plays on each night. The color indicates the
     position in the set a song is played. {#if $isMobile} Tap on{:else}Hover over{/if} a square to listen to the song and see that show's entire setlist. This app will update as new setlists become available.</p>
 
-  <p>See anything off? Have ideas on how to make this better? Send me an email at <a href="mailto:jared.m.whalen@gmail.com">jared.m.whalen@gmail.com</a></p>
+  <p>See anything off? Have ideas on how to make this better? Send me an email at <a href="mailto:jared.m.whalen@gmail.com">jared.m.whalen@gmail.com</a>.</p>
 
 </div>
 
